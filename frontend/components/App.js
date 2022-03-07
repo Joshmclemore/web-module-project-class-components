6d25ed30aae81a6f4ac26767eb797d11120d8e90
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-07 16:07:59 -0600

    Basic DOM elements are in place, but all are located in App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,53 @@
 import React from 'react'
 
+
+let idx = 0;
+const getIdx = () => ++idx
+
+const todos = [
+  { id: getIdx(), name:"Take out the trash", completed: false},
+  { id: getIdx(), name:"Wash the dishes", completed: false},
+  { id: getIdx(), name:"Buy groceries", completed: false}
+]
+
+const initialState = {
+  todos
+}
+
 export default class App extends React.Component {
+
+  state = initialState
+
+  // handleClick
+
+  // handleSubmit
+
+  // filterCompleted
+
+
+
   render() {
     return (
       <div>
-        Todo App
+        <h2>Todos:</h2>
+        <ul> 
+          { 
+            todos.map((todo) => {
+              const { name } = todo
+              return (
+                <li>{name}</li>
+              )
+            })
+          }
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add To List</button>
+        </form>
+
+        <button>Hide Completed</button>
+
       </div>
     )
   }
